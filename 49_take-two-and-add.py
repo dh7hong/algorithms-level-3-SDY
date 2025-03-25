c369acce801ef6d7f714144b1d7651ab9d1a90ab
dh7hong <dh7hong@gmail.com>
2025-03-25 15:23:41 +0900

    03-25

--- 49_take-two-and-add.py
+++ 49_take-two-and-add.py
@@ -13,3 +13,23 @@ def solution(numbers):
     
     # Convert the set to a sorted list and return it
     return sorted(sum_set)
+
+
+def solution2(numbers):
+    answer = []
+    n_length = len(numbers)
+    for i in range(n_length):
+        print(f'i: {i}')
+        for j in range(i + 1, n_length):
+            print(f'i + 1: {i + 1}, j: {j}')
+            print(f'numbers[i]: {numbers[i]}, numbers[j]: {numbers[j]}')    
+            S = numbers[i] + numbers[j]
+            print(f'numbers[i] + numbers[j] = {S}')
+            answer.append(S)
+            print(f'answer.append({S}): {answer}')
+    
+    answer = sorted(set(answer))
+    
+    return answer
+
+print(solution2([2,1,3,4,1]))
